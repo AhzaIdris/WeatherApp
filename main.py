63db7212dcedc1d21fc16aa15ae5e0b6eb6a7c7d
AhzaIdris <ahza.idris@gmail.com>
2024-09-02 21:32:16 +0700

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from flask import Flask, render_template, redirect, url_for
+
+from flask import Flask, render_template,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Blogs.db"
 db = SQLAlchemy(app)
+
 class blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     image = db.Column(db.String(200))
-    post_date = db.Column(db.DataTime)
+    post_date = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
 @app.route('/')
 def index():
     blogposts = blog.query.all()
-    return render_template('index.html', blogposts=blogposts)
+    return render_template("index.html", blogposts =blogposts )
+
 @app.route('/<int:id>')
 def post(id):
     blogpost = blog.query.get(id)
-    return render_template("Post.html", blogpost=blogpost)
+    return render_template("Post.html", blogpost =blogpost)
+
+
 @app.route('/delete/<int:id>', methods = ['GET'])
 def delete(id):
     delpost = blog.query.get(id)
@@ -25,8 +30,7 @@ def delete(id):
     db.session.commit()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
+if __name__=="__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
-
+        app.run()
